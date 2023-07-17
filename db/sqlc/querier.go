5a9b12485d42b07c078f8efb7759e10c8439d4c0
Atharva Kukade <atharva.kukade@icloud.com>
2023-07-16 17:33:56 -0700

    User tests add and fixed errors

--- db/sqlc/querier.go
+++ db/sqlc/querier.go
@@ -20,6 +20,8 @@ type Querier interface {
 	ListEntries(ctx context.Context, arg ListEntriesParams) ([]Entry, error)
 	ListTransfers(ctx context.Context, arg ListTransfersParams) ([]Transfer, error)
 	UpdateAccount(ctx context.Context, arg UpdateAccountParams) (Account, error)
+	CreateUser(ctx context.Context, arg CreateUserParams) (User, error)
+	GetUser(ctx context.Context, username string) (User, error)
 }
 
 var _ Querier = (*Queries)(nil)
